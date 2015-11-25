88cce75e687e32ef569f3dfcd319183acedb207f
Rebecca Appleyard <rklappleyard@gmail.com>
2015-11-25 14:51:29 +0000

    Shows no results if search cleared

--- test/e2e/GiveTheGovernmentABurritoFeature.js
+++ test/e2e/GiveTheGovernmentABurritoFeature.js
@@ -2,6 +2,7 @@ describe('Find My Lunch', function() {
   var searchBox = element(by.model('searchTerm'));
   var searchButton = element(by.className('get-lunch-btn'));
   var searchedFor = element(by.className('searched_for'));
+  var meals = element.all(by.repeater('item in searchResult'));
 
   beforeEach(function() {
     browser.get('http://localhost:8080');
@@ -14,11 +15,15 @@ describe('Find My Lunch', function() {
   it('finds food and displays the search term, meal names, cost, image and vendor', function() {
     searchBox.sendKeys('burritos');
     searchButton.click();
-    var meals = element.all(by.repeater('item in searchResult'));
     expect(searchedFor.getText()).toEqual('Search results for burritos');
     expect(meals.get(0).element(by.binding('item.name')).getText()).toEqual('Bombay Burritos and Poppadoms');
     expect(meals.get(0).element(by.binding('item.totalGrossFoodCost')).getText()).toEqual('7.2');
     expect(meals.get(0).element(by.binding('item.vendor.name')).getText()).toEqual('Bombay Burrito');
     expect(meals.get(0).element(by.tagName('img')).getAttribute('src')).toEqual('https://uploads.citypantry.com/images/package/2015-04/medium-917295721bb4b238777b6296a760e81958126887.jpg');
+  });
+
+  it('clear search if search term deleted', function() {
+    searchBox.sendKeys('');
+    expect(meals.count()).toBe(0);
   });
 });
