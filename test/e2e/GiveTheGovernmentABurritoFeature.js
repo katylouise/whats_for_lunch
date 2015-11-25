edd1f6a3d05f4e3a48d03feba4c20a5be81c1ecd
Rebecca Appleyard <rklappleyard@gmail.com>
2015-11-25 14:19:53 +0000

    Protractor test passing - meal names displaying on page along with search term

--- test/e2e/GiveTheGovernmentABurritoFeature.js
+++ test/e2e/GiveTheGovernmentABurritoFeature.js
@@ -1,7 +1,21 @@
 describe('Find My Lunch', function() {
-  it('has a title', function() {
-    browser.get('http://localhost:8080');
+  var searchBox = element(by.model('searchTerm'));
+  var searchButton = element(by.className('get-lunch-btn'));
+  var searchedFor = element(by.className('searched_for'));
 
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
+  });
+
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('Find My Lunch');
+  });
+
+  it('finds food and shows the search term', function() {
+    searchBox.sendKeys('burritos');
+    searchButton.click();
+    var meals = element.all(by.repeater('item in searchResult'));
+    expect(searchedFor.getText()).toEqual("Search results for burritos");
+    expect(meals.get(0).element(by.binding('item.name')).getText()).toEqual('Bombay Burritos and Poppadoms');
   });
 });
