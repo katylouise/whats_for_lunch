255e85fb8c1cf68a8901fdff0e1e0d5c9d6974e1
Rebecca Appleyard <rklappleyard@gmail.com>
2015-11-25 14:38:06 +0000

    Protractor test passing - correctly showing name, vendor, image and cost

--- test/e2e/GiveTheGovernmentABurritoFeature.js
+++ test/e2e/GiveTheGovernmentABurritoFeature.js
@@ -11,11 +11,14 @@ describe('Find My Lunch', function() {
     expect(browser.getTitle()).toEqual('Find My Lunch');
   });
 
-  it('finds food and shows the search term', function() {
+  it('finds food and displays the search term, meal names, cost, image and vendor', function() {
     searchBox.sendKeys('burritos');
     searchButton.click();
     var meals = element.all(by.repeater('item in searchResult'));
-    expect(searchedFor.getText()).toEqual("Search results for burritos");
+    expect(searchedFor.getText()).toEqual('Search results for burritos');
     expect(meals.get(0).element(by.binding('item.name')).getText()).toEqual('Bombay Burritos and Poppadoms');
+    expect(meals.get(0).element(by.binding('item.totalGrossFoodCost')).getText()).toEqual('7.2');
+    expect(meals.get(0).element(by.binding('item.vendor.name')).getText()).toEqual('Bombay Burrito');
+    expect(meals.get(0).element(by.tagName('img')).getAttribute('src')).toEqual('https://uploads.citypantry.com/images/package/2015-04/medium-917295721bb4b238777b6296a760e81958126887.jpg');
   });
 });
